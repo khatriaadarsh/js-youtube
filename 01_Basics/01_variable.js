425b11b0f4dafa79639a89a116a632ecc52e3743
Aadarsh Kumar <139700003+khatriaadarsh@users.noreply.github.com>
2023-12-15 06:50:05 +0000

    Variable completed

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,10 @@
+const studentId = 122445
+let studentEmail = "aadarshkumar9503@gmail.com"
+var idPassword = "Span123"
+studentCity = "Saigen"
+
+//studentId = 4423 in const same variable we does not assign again value
+
+studentEmail = "@gmail.com"
+idPassword = "Pakistan123"
+studentCity = "Iceland"
